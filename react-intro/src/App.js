2349a0917ec0457f3665d23ec44f83f075a3432e
Sara <sarailic431@gmail.com>
2025-04-20 22:15:35 +0200

    .

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -5,15 +5,49 @@ import React from "react";
 import "./App.css";
 import Employee from "./Employee";
 
+const employeeInfo = [
+  {
+    firstName: "Jack",
+    lastName: "Smith",
+    age: "21",
+    employeeId: "1",
+  },
+  {
+    firstName: "Stephanie",
+    lastName: "A",
+    age: "21",
+    employeeId: "2",
+  },
+  {
+    firstName: "George",
+    lastName: "B",
+    age: "21",
+    employeeId: "3",
+  },
+  {
+    firstName: "Anthony",
+    lastName: "C",
+    age: "21",
+    employeeId: "4",
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> Company Directory </h1>
-        <Employee firstName="Jack" lastName="Smith" age="21" />
+
+        {employeeInfo.map((employee) => {
+          // const { firstName, lastName, age } = employee;
+
+          return <Employee key={employee.employeeId} {...employee} />;
+        })}
+
+        {/* <Employee firstName="Jack" lastName="Smith" age="21" />
         <Employee firstName="Stephanie" lastName="A" age="21" />
         <Employee firstName="George" lastName="B" age="21" />
-        <Employee firstName="Anthony" lastName="C" age="21" />
+        <Employee firstName="Anthony" lastName="C" age="21" /> */}
       </header>
     </div>
   );
