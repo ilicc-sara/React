ad72e331711f66457ac3b8ffbe053e0e4a1360de
Sara <sarailic431@gmail.com>
2025-04-20 22:52:47 +0200

    lection 4 finished

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -5,50 +5,87 @@ import React from "react";
 import "./App.css";
 import Employee from "./Employee";
 
-const employeeInfo = [
-  {
-    firstName: "Jack",
-    lastName: "Smith",
-    age: "21",
-    employeeId: "1",
+// const employeeInfo = [
+//   {
+//     firstName: "Jack",
+//     lastName: "Smith",
+//     age: "21",
+//     employeeId: "1",
+//   },
+//   {
+//     firstName: "Stephanie",
+//     lastName: "A",
+//     age: "21",
+//     employeeId: "2",
+//   },
+//   {
+//     firstName: "George",
+//     lastName: "B",
+//     age: "21",
+//     employeeId: "3",
+//   },
+//   {
+//     firstName: "Anthony",
+//     lastName: "C",
+//     age: "21",
+//     employeeId: "4",
+//   },
+// ];
+
+const employee = {
+  id: "ABC",
+  name: "Anthony",
+  salary: "$10",
+  address: {
+    salary: "$100",
+    street: "210 Washington Street",
+    country: "Canada",
+    province: "Ontario",
+    zones: {
+      plantZone: "7B",
+      alienZone: "41",
+    },
   },
-  {
-    firstName: "Stephanie",
-    lastName: "A",
-    age: "21",
-    employeeId: "2",
-  },
-  {
-    firstName: "George",
-    lastName: "B",
-    age: "21",
-    employeeId: "3",
-  },
-  {
-    firstName: "Anthony",
-    lastName: "C",
-    age: "21",
-    employeeId: "4",
-  },
-];
+};
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <h1> Company Directory </h1>
+
+  //       {employeeInfo.map((employee) => {
+  //         // const { firstName, lastName, age } = employee;
+
+  //         return <Employee key={employee.employeeId} {...employee} />;
+  //       })}
+
+  //       {/* <Employee firstName="Jack" lastName="Smith" age="21" />
+  //       <Employee firstName="Stephanie" lastName="A" age="21" />
+  //       <Employee firstName="George" lastName="B" age="21" />
+  //       <Employee firstName="Anthony" lastName="C" age="21" /> */}
+  //     </header>
+  //   </div>
+  // );
+
+  const {
+    name,
+    id,
+    address: {
+      street,
+      country,
+      province,
+      zones: { plantZone: myPlantZone, alienZone },
+    },
+  } = employee;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Company Directory </h1>
-
-        {employeeInfo.map((employee) => {
-          // const { firstName, lastName, age } = employee;
-
-          return <Employee key={employee.employeeId} {...employee} />;
-        })}
-
-        {/* <Employee firstName="Jack" lastName="Smith" age="21" />
-        <Employee firstName="Stephanie" lastName="A" age="21" />
-        <Employee firstName="George" lastName="B" age="21" />
-        <Employee firstName="Anthony" lastName="C" age="21" /> */}
-      </header>
+      <h2>{name}</h2>
+      <h2>{id}</h2>
+      <h2>
+        {street} {country} {province} {myPlantZone} {alienZone}
+      </h2>
     </div>
   );
 }
